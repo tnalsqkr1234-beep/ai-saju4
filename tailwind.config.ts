539c3eb01590f04127cd96d1ac1fe2857dc52ad3
tnalsqkr1234-beep <tnalsqkr1234@gmail.com>
2025-08-20 17:31:13 +0900

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "tailwindcss";
-export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: { boxShadow:{soft:"0 10px 25px rgba(0,0,0,.08)"}, borderRadius:{'2xl':"1rem"} } },
-  plugins: [],
-} satisfies Config;
